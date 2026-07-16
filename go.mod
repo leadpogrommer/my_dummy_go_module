edbf8ea8e227e9d3350b71de9eee49000d3e1059
leadpogrommer <ilyamerzlakov@gmail.com>
2026-07-16 16:12:10 +0700

    v1.5.0

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/leadpogrommer/my_dummy_go_module
 
 go 1.26.3
+
+require github.com/samber/lo v1.666.727
+
+require golang.org/x/text v0.22.0 // indirect
--- hello/hello.go
+++ hello/hello.go
@@ -2,11 +2,11 @@ package hello
 
 import (
 	"fmt"
-	// "github.com/samber/lo"
+	"github.com/samber/lo"
 )
 
 func GetHello(name string) string {
 
-	// fmt.Println(lo.Map([]byte(name), func(b byte, _ int) byte { return b + 1 }))
+	fmt.Println(lo.Map([]byte(name), func(b byte, _ int) byte { return b + 1 }))
 	return fmt.Sprintf("Hello, %s (v1.3.0)", name)
 }
